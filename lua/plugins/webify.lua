5f0778c2ce523a151aa8e30b30f479102cce64c1
yuval <3dyuval@gmail.com>
2025-09-22 06:50:48 +0300

    added browsing plugins and maps

---
+++ lua/plugins/webify.lua
@@ -0,0 +1,5 @@
+-- Webify.nvim - Open current file in GitHub/GitLab web interface
+return {
+  "pabloariasal/webify.nvim",
+  cmd = { "OpenFileInRepo", "OpenLineInRepo", "YankFileUrl", "YankLineUrl" },
+}
