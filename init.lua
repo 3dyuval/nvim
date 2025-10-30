855954f8c47f18d7fbb0786432ab4802dbd5b2d2
yuval <3dyuval@gmail.com>
2025-10-31 00:44:56 +0200

    added explicit node path

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
+-- Add NVM node to PATH before plugins load (required for Treesitter + LSP)
+vim.env.PATH = vim.env.HOME .. "/.nvm/versions/node/v20.19.3/bin:" .. vim.env.PATH
+
 require("config.lazy")
 require("utils.diff-highlights")
