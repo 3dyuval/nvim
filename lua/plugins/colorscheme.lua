65aadbe653e6223c87f12ed0198a3f2d73d924ba
Yuval.D <yuvddd@pm.me>
2025-11-25 17:45:49 +0200

    resolve conflicts

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,25 +2,22 @@
 -- This file is managed by omarchy-theme-set-neovim
 
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		opts = {
-<<<<<<< Updated upstream:lua/plugins/colorscheme.lua
-			transparent_background = true,
-=======
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
->>>>>>> Stashed changes:lua/plugins/colorscheme-persist.lua
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "tokyonight-night",
-		},
-	},
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
 }
