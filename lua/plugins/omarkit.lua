53cd0a12fa575a4b66ac23a4ed975d54e0e50143
Yuval.D <yuvddd@pm.me>
2026-03-18 15:29:46 +0200

    feat: rename omarkitty plugin spec to omarkit, point to ~/omarkit.nvim

---
+++ lua/plugins/omarkit.lua
@@ -0,0 +1,7 @@
+return {
+  dir = vim.fn.expand('~/omarkit.nvim'),
+  lazy = false,
+  config = function()
+    require('omarkit').setup()
+  end,
+}
--- lua/plugins/omarkitty.lua
+++
@@ -1,7 +0,0 @@
-return {
-  dir = vim.fn.expand('~/.local/share/omarchy/plugins/omarkitty.nvim'),
-  lazy = false,
-  config = function()
-    require('omarkitty').setup()
-  end,
-}
