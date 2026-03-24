b11c3ac0cc4341b2a5f9b6b6f660b15b48ea6aba
Yuval.D <yuvddd@pm.me>
2026-03-24 12:48:29 +0200

    feat: add nvzone/menu dependency and demo copy-date action

--- lua/plugins/activity-heatmap.lua
+++ lua/plugins/activity-heatmap.lua
@@ -3,7 +3,18 @@ return {
     "3dyuval/activity-heatmap.nvim",
     dir = "/home/yuv/proj/activity-heatmap.nvim",
     dev = true,
-    dependencies = { "nvzone/volt" },
-    opts = {},
+    dependencies = { "nvzone/volt", "nvzone/menu" },
+    opts = {
+      actions = {
+        {
+          label  = "Copy date",
+          key    = "y",
+          action = function(item)
+            vim.fn.setreg("+", item.date)
+            vim.notify("Copied: " .. item.date)
+          end,
+        },
+      },
+    },
   },
 }
