f08acd0156a04db7b633086ae4e319ba4aa80ad9
Yuval.D <yuvddd@pm.me>
2026-04-07 14:06:19 +0300

    fix: missing dev = true for seaxng

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,7 +16,7 @@ end
 return {
   {
     "folke/snacks.nvim",
-    dependencies = { "3dyuval/searxng.nvim" },
+    dependencies = { "3dyuval/searxng.nvim", dev = true },
     priority = 1000,
     lazy = false,
     ---@type snacks.Config
