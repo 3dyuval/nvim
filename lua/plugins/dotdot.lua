4f8e5b4cdfe3dd497354f094fb2cacc18b35eabd
Yuval.D <yuvddd@pm.me>
2026-05-20 18:19:51 +0300

    fix(dotdot): Fix plugin configuration structure and add InsertEnter event with manual trigger

--- lua/plugins/dotdot.lua
+++ lua/plugins/dotdot.lua
@@ -2,29 +2,39 @@ return {
   {
     "hernandez/dotdot.nvim",
     url = "https://codeberg.org/hernandez/dotdot.nvim",
-    dependencies = { "folke/snacks.nvim" },
-    opts = function()
-      local px = require("utils.picker-extensions")
-      return {
-        insert_trigger = "..",
-        adapter = "snacks",
-        commands = {
-          {
-            id = "explorer",
-            title = "Open Explorer",
-            run = function()
-              px.open_explorer()
-            end,
-          },
-          {
-            id = "git_conflicts",
-            title = "Git Conflicts",
-            run = function()
-              px.git_conflicts()
-            end,
-          },
-        },
+    dependencies = {"folke/snacks.nvim"},
+    event = "InsertEnter",
+    keys = {
+      {
+        "<C-,>",
+        function()
+          require("dotdot").show()
+        end,
+        mode = "i",
+        desc = "dotdot commands"
       }
-    end,
-  },
+    },
+    opts = {
+      adapter = "snacks",
+      insert_trigger = "..",
+      commands = {
+        {
+          id = "explorer",
+          title = "Open Explorer",
+          category = "FILES",
+          run = function(ctx)
+            require("utils.picker-extensions").open_explorer()
+          end
+        },
+        {
+          id = "git_conflicts",
+          title = "Git Conflicts",
+          category = "GIT",
+          run = function(ctx)
+            require("utils.picker-extensions").git_conflicts()
+          end
+        }
+      }
+    }
+  }
 }
