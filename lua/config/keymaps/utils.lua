24d96c503b2d40fcc3ed029370f4874e52e47506
Yuval.D <yuvddd@pm.me>
2026-06-07 18:02:49 +0300

    chore: add utils.lua for keymap utilities

---
+++ lua/config/keymaps/utils.lua
@@ -0,0 +1,8 @@
+-- [nfnl] fnl/config/keymaps/utils.fnl
+local lset = vim.keymap.set
+local ufo = require("ufo")
+lset("<leader>gs", ":DiffviewOpen %", {desc = "File DiffviewOpen history", noremap = true})
+lset("ff", ufo.action.openFoldsWith(0), {desc = "Close all folds (fold all)", noremap = true})
+lset("fF", "zr", {desc = "Open all folds (unfold all)", noremap = true})
+lset("fo", "zo", {desc = "Open fold (unfold)", noremap = true})
+return lset("fu", "zc", {desc = "Close fold (fold one)", noremap = true})
