6518cf73c6e0a9578bde17618f54b0e1fa9865f6
Yuval.D <yuvddd@pm.me>
2026-06-17 13:55:18 +0300

    feat: gitgraph

--- fnl/config/keymaps/utils.fnl
+++ fnl/config/keymaps/utils.fnl
@@ -1,15 +1,24 @@
 (local lset vim.keymap.set)
 
-(lset :n :<leader>gg
+(lset :n :<leader>gG ":Gitsigns"
+      {:desc "Gitsigns prefill"})
+
+(lset :n :<leader>gg ":DiffviewGraph<CR>"
+      {:desc "Gitsigns prefill"})
+
+(lset :n :<leader>gn
       (fn [] (vim.cmd (.. "Neogit kind=vsplit cwd=" (vim.fn.expand "%:p:h"))))
-      {:desc "Neogit (side)" :noremap true})
+      {:desc "Neogit (side)"})
 
 (lset :n :<leader>gc
-      (fn [] (vim.cmd "Neogit commit"))
-      {:desc "Neogit commit" :noremap true})
+      ":Neogit commit<CR>"
+      {:desc "Neogit commit"})
 
 
 (lset :n :<leader>gs ":DiffviewOpen %"
-      {:desc "File DiffviewOpen history" :noremap true})
+      {:desc "File DiffviewOpen history"})
+
+(lset :n :<leader>gh (fn [] (vim.cmd "DiffviewFileHistory ."))
+      {:desc "Diffview repo log"})
 (lset :n :P (fn [] ((. (require :hover) :open)))
                    {:desc "Hover"})
--- lua/config/keymaps/utils.lua
+++ lua/config/keymaps/utils.lua
@@ -1,20 +1,18 @@
 -- [nfnl] fnl/config/keymaps/utils.fnl
 local lset = vim.keymap.set
-lset("n", "<leader>gg", ":Gitsigns", {desc = "Gitsigns prefill", noremap = true})
+lset("n", "<leader>gG", ":Gitsigns", {desc = "Gitsigns prefill"})
+lset("n", "<leader>gg", ":DiffviewGraph<CR>", {desc = "Gitsigns prefill"})
 local function _1_()
   return vim.cmd(("Neogit kind=vsplit cwd=" .. vim.fn.expand("%:p:h")))
 end
-lset("n", "<leader>gG", _1_, {desc = "Neogit (side)", noremap = true})
+lset("n", "<leader>gn", _1_, {desc = "Neogit (side)"})
+lset("n", "<leader>gc", ":Neogit commit<CR>", {desc = "Neogit commit"})
+lset("n", "<leader>gs", ":DiffviewOpen %", {desc = "File DiffviewOpen history"})
 local function _2_()
-  return vim.cmd("Neogit commit")
-end
-lset("n", "<leader>gc", _2_, {desc = "Neogit commit", noremap = true})
-lset("n", "<leader>gs", ":DiffviewOpen %", {desc = "File DiffviewOpen history", noremap = true})
-local function _3_()
   return vim.cmd("DiffviewFileHistory .")
 end
-lset("n", "<leader>gh", _3_, {desc = "Diffview repo log", noremap = true})
-local function _4_()
+lset("n", "<leader>gh", _2_, {desc = "Diffview repo log"})
+local function _3_()
   return require("hover").open()
 end
-return lset("n", "P", _4_, {desc = "Hover"})
+return lset("n", "P", _3_, {desc = "Hover"})
